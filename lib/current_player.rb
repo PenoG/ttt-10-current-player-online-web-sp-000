711c4521752b5afad04b56f273e8c163a1b5ec3b
Mason Phillips <mjphillips2000@gmail.com>
2020-09-30 14:41:21 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,9 @@
 def turn_count(board,index)
   count = 0
   if board.each do |token|
-    if token == "X" or token == "O"
-      count +=
+    if token == "X" || token == "O"
+      count += 1
+    end
   end
 end
 
